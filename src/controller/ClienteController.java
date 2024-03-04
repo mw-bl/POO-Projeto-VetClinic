f26f8a85c5d3e3d27fa98f16064702f0f6cea799
mw-bl <bezerram533@gmail.com>
2024-03-04 16:50:28 -0300

    ClienteController.java

---
+++ src/controller/ClienteController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import models.Cliente;
+
+public class ClienteController {
+    // CRUD
+    public static void insertData(Connection conn, Cliente cliente) throws SQLException {
+        String sql = "INSERT INTO Cliente (nome, telefone) VALUES (?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, cliente.getNome());
+            pstmt.setString(2, cliente.getTelefone());
+            pstmt.executeUpdate();
+            System.out.println("Cliente cadastrado com sucesso.");
+        }
+    }
+
+    public static ArrayList<Cliente> selectData(Connection conn) throws SQLException {
+        String sql = "SELECT * FROM Cliente";
+        ArrayList<Cliente> listClientes = new ArrayList<>();
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNome(rs.getString("nome"));
+                cliente.setTelefone(rs.getString("telefone"));
+                listClientes.add(cliente);
+            }
+        }
+        return listClientes;
+    }
+
+    public static void updateData(Connection conn, Cliente cliente) throws SQLException {
+        String sql = "UPDATE Cliente SET nome = ?, telefone = ? WHERE id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, cliente.getNome());
+            pstmt.setString(2, cliente.getTelefone());
+            pstmt.setInt(3, cliente.getId());
+            pstmt.executeUpdate();
+            System.out.println("Dados do cliente atualizados com sucesso.");
+        }
+    }
+
+    public static void deleteData(Connection conn, int clienteId) throws SQLException {
+        String sql = "DELETE FROM Cliente WHERE id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, clienteId);
+            pstmt.executeUpdate();
+            System.out.println("Cliente excluído com sucesso.");
+        }
+    }
+}
