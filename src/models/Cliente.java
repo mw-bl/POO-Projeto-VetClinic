839741ffd83625a7df8df0c840a82a107ea668fe
mw-bl <bezerram533@gmail.com>
2024-03-04 15:39:26 -0300

    Cliente.java

---
+++ src/models/Cliente.java
@@ -0,0 +1,35 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private String telefone;
+
+    public Cliente (int id, String nome, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    // Getter/Setter - ID
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter/Setter - Nome
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter/Setter - Telefone
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
