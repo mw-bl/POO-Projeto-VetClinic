fde45c550402fbcad7aa4f14cd64cec04078b4e3
mw-bl <bezerram533@gmail.com>
2024-03-04 15:48:40 -0300

    Conexao.java

---
+++ src/db/Conexao.java
@@ -0,0 +1,26 @@
+package db;
+
+import java.sql.Connection; 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Para conexão com o banco (versao 8.0.30): https://jar-download.com/artifacts/mysql/mysql-connector-java
+
+public class Conexao {
+    public static java.sql.Connection getConexaoMySQL() {
+        Connection connection = null;
+        String serverName = "localhost";
+        String mydatabase = "VetClinic";
+
+        String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+        String username = "root";
+        String password  = "mw123";
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
