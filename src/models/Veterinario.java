f24c26a711cc07b580d4253c0bb338c432e09b99
mw-bl <bezerram533@gmail.com>
2024-03-04 15:36:44 -0300

    Veterinario.java

---
+++ src/models/Veterinario.java
@@ -0,0 +1,45 @@
+public class Veterinario {
+    private int id;
+    private String nome;
+    private String especialidade;
+    private String telefone;
+
+    public Veterinario (int id, String nome, String especialidade, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.telefone = telefone;
+    }
+
+    // Getter/Setter - ID
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter/Setter - Nome
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter/Setter - Especialidade
+    public String getEspecialidade() {
+        return especialidade;
+    }
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    // Getter/Setter - Telefone
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
