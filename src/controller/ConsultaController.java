11b50a6781ffcd36d8f56c68b5b629dfa6066c76
mw-bl <bezerram533@gmail.com>
2024-03-04 16:50:46 -0300

    ConsultaController.java

--- src/controller/ConsultaController.java
+++ src/controller/ConsultaController.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import models.Consulta;
 
 public class ConsultaController {
+    // CRUD
     public static void insertData(Connection conn, Consulta consulta) throws SQLException {
         String sql = "INSERT INTO Consulta (dataHora, veterinario_id, pet_id, notas) VALUES (?, ?, ?, ?)";
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -22,24 +23,24 @@ public class ConsultaController {
         }
     }
 
-    public static ArrayList selectData(Connection conn) throws SQLException {
+    public static ArrayList<Consulta> selectData(Connection conn) throws SQLException {
         String sql = "SELECT * FROM Consulta";
-        ArrayList<Consulta> listConsulta = new ArrayList<>();
+        ArrayList<Consulta> listConsultas = new ArrayList<>();
         try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
+            ResultSet rs = stmt.executeQuery(sql)) {
                 while (rs.next()) {
-                    Consulta c = new Consulta();
-                    c.setId(rs.getInt("id"));
-                    c.setDataHora(rs.getString("DataHora"));
+                    Consulta consulta = new Consulta();
+                    consulta.setId(rs.getInt("id"));
+                    consulta.setDataHora(rs.getString("DataHora"));
                     // Preencha as referências para Veterinário e Pet conforme necessário
-                    c.setNotas(rs.getString("notas"));
-                    listConsulta.add(c);
+                    consulta.setNotas(rs.getString("notas"));
+                    listConsultas.add(consulta);
                 }
         }
-        return listConsulta;
+        return listConsultas;
     }
 
-    public static void update(Connection conn, Consulta consulta) throws SQLException {
+    public static void updateData(Connection conn, Consulta consulta) throws SQLException {
         String sql = "UPDATE Consulta SET dataHora = ?, veterinario_id = ?, pet_id = ?, notas = ? WHERE id = ?";
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, consulta.getDataHora());
@@ -52,7 +53,7 @@ public class ConsultaController {
         }
     }
 
-    public static void delete(Connection conn, int consultaId) throws SQLException {
+    public static void deleteData(Connection conn, int consultaId) throws SQLException {
         String sql = "DELETE FROM Consulta WHERE id = ?";
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, consultaId);
