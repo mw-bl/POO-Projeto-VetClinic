478b5099e7b42112bd7e07b70c801f3836abce6f
mw-bl <bezerram533@gmail.com>
2024-03-07 16:53:23 -0300

    Update Tutor.java

---
+++ src/models/Tutor.java
@@ -0,0 +1,59 @@
+package models;
+
+public class Tutor {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String dataNascimento;
+
+    public Tutor() {
+    }
+
+    public Tutor (int id, String nome, String telefone, String email, String dataNascimento) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+    }
+
+    // Getter/Setter - ID
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter/Setter - Nome
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter/Setter - Telefone
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    // Getter/Setter - Email
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter/Setter - Data de Nascimento
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
